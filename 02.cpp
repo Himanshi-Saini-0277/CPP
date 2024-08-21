5edee768bb1bb49f9d9f18b38f06a6ac7619e31a
Himanshi <himanshidosarka@gmail.com>
2024-08-21 19:12:20 +0530

    c++

---
+++ 02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// instead of using std in front of every cin and cout just use the above line
+
+/* 
+this
+is 
+a
+multiline 
+comment
+*/
+
+int main(){
+    cout<<"Hi, I am Himanshi";
+    return 0;
+}
