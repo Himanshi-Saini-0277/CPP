550baceba01c2ca2bda1deefccfb97c8235a19c9
Himanshi <himanshidosarka@gmail.com>
2024-08-22 19:59:14 +0530

    c++

---
+++ 03_data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=5, b=5;
+    cout << a+b << endl;
+
+    float c = 3.14;
+    cout << "Value of pi is: " << c << endl;
+
+    char d = 'H';
+    cout << d << endl;
+
+    bool e = true;
+    cout << e << endl;
+
+    return 0;
+}
