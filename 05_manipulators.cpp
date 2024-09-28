bd344b67d62c28a44f81939b1345541c1f8d4ac5
Himanshi <himanshidosarka@gmail.com>
2024-09-28 16:17:12 +0530

    C++

---
+++ 05_manipulators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    // endl
+    int a =10, b =200;
+    cout << "Value of a is: "<< a << endl;
+    cout << "Value of b is: "<< b << endl;
+
+    //setw
+    cout << "Setw value of a: " << setw(4)<< a << endl;
+    cout << "Setw value of b: " << setw(4)<< b << endl;
+
+}
